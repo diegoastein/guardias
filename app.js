a2665eedab3b2f9fc30d97c446a5df5b08b8d386
diegoastein <112906670+diegoastein@users.noreply.github.com>
2025-12-18 18:30:18 -0300

    js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+    import { getFirestore, collection, doc, setDoc, deleteDoc, onSnapshot, writeBatch } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+    import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
+    const { useState, useEffect, useMemo, useRef } = React;
+    const { jsPDF } = window.jspdf;
